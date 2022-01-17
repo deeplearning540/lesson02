fb78568307e84f1b9ba4a95fe13f851a8d189173
Peter Steinbach <p.steinbach@hzdr.de>
2022-01-17 14:20:08 +0100

    blowing up the blobs a bit so that clustering becomes harder

--- data/datasets.py
+++ data/datasets.py
@@ -1,23 +1,33 @@
-from sklearn.datasets import make_blobs
 import numpy as np
 import pandas as pd
+from sklearn.datasets import make_blobs
+
 
 def main():
+    nfeats = 4
+    rng = np.random.default_rng(12345)
+
     for i in range(10):
-        std = .4+i*.04
-        X, y = make_blobs(n_samples=1000,
-                          centers=4,
-                          n_features=4,
-                          cluster_std=std,
-                          random_state=i)
+        std = rng.uniform(1, 4, nfeats)
+        X, y = make_blobs(
+            n_samples=1000, centers=4, n_features=4, cluster_std=std, random_state=i
+        )
 
         label = (y > 1).astype(np.int8)
 
-        d = {'x1' : X[:,0],'x2': X[:,1], 'x3': X[:,2], 'x4': X[:,3],'label': label,'raw':y}
+        d = {
+            "x1": X[:, 0],
+            "x2": X[:, 1],
+            "x3": X[:, 2],
+            "x4": X[:, 3],
+            "label": label,
+            "raw": y,
+        }
         df = pd.DataFrame(data=d)
         fn = "clustering_data_{:02.0f}.csv".format(i)
-        print(df.shape,"->",fn)
+        print(df.shape, "->", fn)
         df.to_csv(fn)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
